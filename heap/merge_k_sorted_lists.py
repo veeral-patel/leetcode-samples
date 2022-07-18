bdc949b10912bd749e1ab2b4363b46f759741e5c
Veeral Patel <veeralpatel@Veerals-MacBook-Pro-2.local>
2022-07-18 16:03:01 -0700

    add code for merge k sorted lists

--- heap/merge_k_sorted_lists.py
+++ heap/merge_k_sorted_lists.py
@@ -1,5 +1,25 @@
 from typing import List
 
 def merge_k_sorted_lists(lists: List[List[int]]) -> List[int]:
-    # WRITE YOUR BRILLIANT CODE HERE
-    return []
+    # https://algo.monster/problems/merge_k_sorted_lists
+    # Store (value, index of list in list of lists, index of value in that list)
+    # in heap.
+    heap = []
+
+    for listIndex, lst in enumerate(lists):
+        heapq.heappush(heap, (lst[0], listIndex, 0))
+
+    ans = []
+
+    while heap:
+        val, listIndex, index = heapq.heappop(heap)
+
+        ans.append(val)
+
+        lst = lists[listIndex]
+
+        if index < len(lst)-1:
+            heapq.heappush(heap, (lst[index+1], listIndex, index+1))
+
+    return ans
+
